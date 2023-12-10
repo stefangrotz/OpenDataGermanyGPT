316d5fd4fd5d46ecb80ee4041e82d9d6cb71fa85
Stefan R. Grotz <stefan.grotz@posteo.de>
2023-12-10 14:16:26 +0100

    Using Wikidata directly

--- actions/create-api-files.py
+++ actions/create-api-files.py
@@ -1,4 +1,39 @@
+import requests
 import json
+
+# SPARQL query to fetch data from Wikidata
+sparql_query = """
+SELECT ?portal ?portalLabel ?countryLabel ?hasApiEndpoint ?website ("CKAN" AS ?protocol)  # Adding CKAN as a static value for protocol
+WHERE {
+  ?portal wdt:P31 wd:Q27031827;                        # Searching for instances of Open Data Portals
+          wdt:P17 ?country.                            # Restricting to a country, variable ?country
+          VALUES ?country { wd:Q183 wd:Q40 wd:Q39 }    # Germany (Q183), Austria (Q40), Switzerland (Q39)
+          OPTIONAL { ?portal wdt:P6269 ?hasApiEndpoint }   # Searching for API Endpoint properties (if available)
+          OPTIONAL { ?portal wdt:P6269 ?apiEndpoint.    # Searching for API Endpoint properties (if available)
+                     ?apiEndpoint wdt:P2700 wd:Q105592698;   # Ensuring the API uses the CKAN protocol
+                     BIND(CONCAT(STR(?apiEndpoint)) AS ?hasApiEndpoint) }
+          OPTIONAL { ?portal wdt:P856 ?website }           # Searching for the official website (if available)
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". 
+                           ?portal rdfs:label ?portalLabel .
+                           ?country rdfs:label ?countryLabel . }
+}
+ORDER BY DESC(?hasApiEndpoint)
+"""
+
+# Wikidata SPARQL endpoint
+endpoint_url = "https://query.wikidata.org/sparql"
+
+# Make the request to Wikidata
+response = requests.get(endpoint_url, params={'query': sparql_query, 'format': 'json'})
+data = response.json()
+# Print a small part of the data to inspect its structure
+print(data['results']['bindings'][:2])  # Print the first two items for inspection
+
+
+# Process the data (assuming it's in the format you expect)
+portals = [item for item in data['results']['bindings']]
+
+
 
 # Function to create the API structure with dynamic description
 def create_api_structure(url,name):
@@ -257,15 +292,24 @@ def truncate_url(url):
     index = url.find('api/3/')
     return url[:index + 6] if index != -1 else url
 
-# Load the JSON data from the file
-with open('query.json', 'r') as file:
-    portals = json.load(file)
+# Process the data (assuming it's in the format you expect)
+portals = [item for item in data['results']['bindings']]
 
-# Prepare a list of endpoints using the loaded JSON data
-endpoints = [
-    {"name": portal["portalLabel"], "url": truncate_url(portal["hasApiEndpoint"])}
-    for portal in portals if "hasApiEndpoint" in portal
-]
+# Try to create endpoints with additional checks and debugging prints
+endpoints = []
+for portal in portals:
+    # Check if necessary keys are present
+    if "portalLabel" in portal and "value" in portal["portalLabel"] and "hasApiEndpoint" in portal and "value" in portal["hasApiEndpoint"]:
+        endpoint = {
+            "name": portal["portalLabel"]["value"], 
+            "url": truncate_url(portal["hasApiEndpoint"]["value"])
+        }
+        endpoints.append(endpoint)
+    else:
+        print(f"Skipping portal due to missing data: {portal}")
+
+# Debug print to check the endpoints list
+print(endpoints)
 
 # Create an API file for each endpoint
 for endpoint in endpoints:
